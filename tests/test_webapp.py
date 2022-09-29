acc1f583b22cd5b0e96f49c4627f9f5c24012e70
Victor Miti <victormiti@umusebo.com>
2022-09-30 00:26:48 +0200

    test: fix broken webapp test

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,4 +1,8 @@
+import os
+
 from flask import url_for
+
+TENLISTS_MP3_CLOUD_STORAGE_BASE_URL = os.getenv("TENLISTS_MP3_CLOUD_STORAGE_BASE_URL")
 
 
 def test_app(client):
@@ -15,16 +19,16 @@ def test_api_valid_route(client):
     """
     res = client.get("/ten-lists/api/v1.0/mp3s?&day=56&format=json")
     mp3_files = [
-        "static/ENGESVC2DA/B03___12_Luke________ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/A02___06_Exodus______ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/B10___05_Ephesians___ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/B27___13_Revelation__ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/A22___02_SongofSongs_ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/A19__056_Psalms______ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/A20___25_Proverbs____ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/A09___07_1Samuel_____ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/A23___56_Isaiah______ENGESVC2DA.mp3",
-        "static/ENGESVC2DA/B05___28_Acts________ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/B03___12_Luke________ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/A02___06_Exodus______ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/B10___05_Ephesians___ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/B27___13_Revelation__ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/A22___02_SongofSongs_ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/A19__056_Psalms______ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/A20___25_Proverbs____ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/A09___07_1Samuel_____ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/A23___56_Isaiah______ENGESVC2DA.mp3",
+        f"{TENLISTS_MP3_CLOUD_STORAGE_BASE_URL}/B05___28_Acts________ENGESVC2DA.mp3",
     ]
     lst = [item["url"] for item in res.json["mp3s"]]
     assert lst == mp3_files
